e98aa3a6c58d8b83b38b2def8db63fab0f2a580a
Gaurav Juvekar <gauravjuvekar@gmail.com>
2018-08-30 08:00:24 -0400

    Update for vertical monitor layout

--- conkyrc.lua
+++ conkyrc.lua
@@ -14,15 +14,15 @@ conky.config = {
 		own_window_class = 'Conky',
 		own_window_type = 'desktop',
 		own_window_transparent = true,
-		own_window_hints = 'undecorated,below,sticky,skip_taskbar,skip_pager',
+        own_window_hints = 'undecorated,below,sticky,skip_taskbar,skip_pager',
 		own_window_argb_visual = true,
 
 		border_inner_margin = 0,
 		border_outer_margin = 0,
 		alignment = 'bottom_left',
-		minimum_width = 1920,
+		minimum_width = 1918,
 		gap_x = 0,
-		gap_y = -420,
+		gap_y = 50,
 
 -- Graphics settings
 		draw_shades = false,
@@ -53,10 +53,10 @@ ${voffset -65}${goto 140}${color FFFFFF}${font Ubuntu:size=50}${time %e}${voffse
 ${voffset -20}${goto 60}${color FFFFFF}${font Ubuntu:size=40}${time %H} : ${time %M}
 ${voffset -23}${goto 60}${color FFFFFF}${font Ubuntu:size=18}${utime } UTC
 ${voffset -140}${color FFFFFF}${font Ubuntu:size=15}${goto 430}CPU${goto 575}MEM${goto 727}DISK${goto 882}ETH${goto 1020}WLAN
-${voffset 35}${color FFFFFF}${font Ubuntu Mono:size=12}${goto 440}${cpu cpu0}%${goto 505}       RAM : ${memperc }%${goto 695}    / : ${fs_used_perc     /}%${goto 865}DS : ${downspeed enp1s0}${goto 1015}DS : ${downspeed wlp2s0}
-${voffset 0 }${color FFFFFF}${font Ubuntu Mono:size=12}${goto 440}${cpu cpu1}%${goto 505}      Swap : ${swapperc}%${goto 695}/home : ${fs_used_perc /home}%${goto 865}US : ${upspeed   enp1s0}${goto 1015}US : ${upspeed   wlp2s0}
-${voffset 0 }${color FFFFFF}${font Ubuntu Mono:size=12}${goto 440}${cpu cpu2}%${goto 505}  Buffered : ${buffers  }${goto 695}    W : ${diskio_write       }${goto 865}TD : ${totaldown enp1s0}${goto 1015}TD : ${totaldown wlp2s0}
-${voffset 0 }${color FFFFFF}${font Ubuntu Mono:size=12}${goto 440}${cpu cpu3}%${goto 505}    Cached : ${cached   }${goto 695}    R : ${diskio_read        }${goto 865}TU : ${totalup   enp1s0}${goto 1015}TU : ${totalup   wlp2s0}
+${voffset 35}${color FFFFFF}${font Ubuntu Mono:size=12}${goto 440}${cpu cpu0}%${goto 505}       RAM : ${memperc }%${goto 695}    / : ${fs_used_perc     /}%${goto 865}DS : ${downspeed enx00e04c361a38}${goto 1015}DS : ${downspeed wlp2s0}
+${voffset 0 }${color FFFFFF}${font Ubuntu Mono:size=12}${goto 440}${cpu cpu1}%${goto 505}      Swap : ${swapperc}%${goto 695}/home : ${fs_used_perc /home}%${goto 865}US : ${upspeed   enx00e04c361a38}${goto 1015}US : ${upspeed   wlp2s0}
+${voffset 0 }${color FFFFFF}${font Ubuntu Mono:size=12}${goto 440}${cpu cpu2}%${goto 505}  Buffered : ${buffers  }${goto 695}    W : ${diskio_write       }${goto 865}TD : ${totaldown enx00e04c361a38}${goto 1015}TD : ${totaldown wlp2s0}
+${voffset 0 }${color FFFFFF}${font Ubuntu Mono:size=12}${goto 440}${cpu cpu3}%${goto 505}    Cached : ${cached   }${goto 695}    R : ${diskio_read        }${goto 865}TU : ${totalup   enx00e04c361a38}${goto 1015}TU : ${totalup   wlp2s0}
 ${voffset -150}${color FFFFFF}${font Ubuntu Mono:size=12}${goto 1150}  Top ${top     pid 1} ${top     name 1}
 ${voffset 0   }${color FFFFFF}${font Ubuntu Mono:size=12}${goto 1150}      ${top     pid 2} ${top     name 2}
 ${voffset 0   }${color FFFFFF}${font Ubuntu Mono:size=12}${goto 1150}      ${top     pid 3} ${top     name 3}
